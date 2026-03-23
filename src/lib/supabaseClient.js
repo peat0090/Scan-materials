b003ff9936c4f8a1862ed1111344ec228978703d
Puttiphong Buala <puttiphong.b@eest-es.com>
2026-03-23 14:05:57 +0700

    fix: use service_role key

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL  = 'https://agvqbdqyfixfwrbkijlx.supabase.co'
-const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFndnFiZHF5Zml4ZndyYmtpamx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNDE2MzIsImV4cCI6MjA4OTgxNzYzMn0.WtIr2yys6y1tjmGGwvmL9Wd6A4ALgPg2IWOTuDYFN1k'
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFndnFiZHF5Zml4ZndyYmtpamx4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDI0MTYzMiwiZXhwIjoyMDg5ODE3NjMyfQ.u-OYcA7Z46XUKz-uNfmzaPxXWbcYfyP2v0juExpUwSw'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
