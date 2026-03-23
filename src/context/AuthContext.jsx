6f646c65b1b721722f27eee2a6f853b370ca4825
Puttiphong Buala <puttiphong.b@eest-es.com>
2026-03-23 14:49:24 +0700

    fix: add jsQR for iOS Safari support

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -55,6 +55,8 @@ export function AuthProvider({ children }) {
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   return useContext(AuthContext)
 }
+
